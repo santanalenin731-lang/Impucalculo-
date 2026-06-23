06ede06613636fac54dcf3470bfdfc0ba057fa4a
Lenins16 <santanalenin731@gmail.com>
2026-06-22 22:51:15 -0400

    Corrige logica de prestaciones laborables y mejora usabilidad de campos de fecha

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'impucalculo-v13';
-const DYNAMIC_CACHE_NAME = 'impucalculo-dynamic-v13';
+const CACHE_NAME = 'impucalculo-v15';
+const DYNAMIC_CACHE_NAME = 'impucalculo-dynamic-v15';
 
 const STATIC_ASSETS = [
     '/',
